8d7d796f0bb6a05eed1a2070d87983d62f6937cd
Phildo <phildo211@gmail.com>
2018-01-17 11:17:28 -0600

    view navigation

--- src/scenes/ui.js
+++ src/scenes/ui.js
@@ -411,7 +411,10 @@ var objectview = function()
 
   self.object;
   self.cur_view = 0;
+  self.cur_view_i = 0;
   self.exit_box = {x:canv.width-100, y:10, w:90, h:90};
+  self.prev_box = {x:10,             y:canv.height/2-50, w:90, h:90};
+  self.next_box = {x:canv.width-100, y:canv.height/2-50, w:90, h:90};
   self.cache_unlocked_views = [];
 
   self.consume_object = function(object)
@@ -420,7 +423,10 @@ var objectview = function()
     self.object.key = true;
     self.unlock_content();
     self.cur_view = self.cache_unlocked_views[0];
-    for(var i = 1; i < self.cache_unlocked_views.length; i++) if(self.cache_unlocked_views[i].primary) cur_view = i;
+    for(var i = 1; i < self.cache_unlocked_views.length; i++) if(self.cache_unlocked_views[i].primary) self.cur_view_i = i;
+    self.cur_view = self.cache_unlocked_views[self.cur_view_i];
+    self.cur_view.key = true;
+    self.unlock_content(); //unlock after viewing "cur"
   }
 
   self.unlock_content = function()
@@ -428,6 +434,11 @@ var objectview = function()
     self.cache_unlocked_views = [];
     for(var i = 0; i < self.object.views.length; i++)
       if(!querylocked(self.object.views[i])) self.cache_unlocked_views.push(self.object.views[i]);
+
+    //re-set self.cur_view_i, ensures cur_view unlocked
+    self.cur_view_i = 0;
+    for(var i = 1; i < self.cache_unlocked_views; i++) if(self.cur_view == self.cache_unlocked_views[i]) self.cur_view_i = i;
+    self.cur_view = self.cache_unlocked_views[self.cur_view_i];
   }
 
   self.click = function(evt)
@@ -439,11 +450,30 @@ var objectview = function()
       cur_state = STATE_TRANSITION;
       state_t = 0;
     }
-    var view = self.cache_unlocked_views[self.cur_view];
-    var zone;
-    for(var i = 0; i < view.zones.length; i++)
+    if(ptWithinBox(self.prev_box,evt.doX,evt.doY))
     {
-      zone = view.zones[i];
+      if(self.cur_view_i == 0) return;
+      else
+      {
+        self.cur_view = self.cache_unlocked_views[self.cur_view_i-1];
+        self.cur_view.key = true;
+        self.unlock_content();
+      }
+    }
+    if(ptWithinBox(self.next_box,evt.doX,evt.doY))
+    {
+      if(self.cur_view_i == self.cache_unlocked_views.length-1) return;
+      else
+      {
+        self.cur_view = self.cache_unlocked_views[self.cur_view_i+1];
+        self.cur_view.key = true;
+        self.unlock_content();
+      }
+    }
+    var zone;
+    for(var i = 0; i < self.cur_view.zones.length; i++)
+    {
+      zone = self.cur_view.zones[i];
       if(ptWithinBox(zone,evt.doX,evt.doY))
       {
         console.log("click zone "+i);
@@ -458,23 +488,25 @@ var objectview = function()
 
   self.draw = function(yoff)
   {
-    var view = self.cache_unlocked_views[self.cur_view];
     var zone;
-    ctx.drawImage(view.img, self.x, self.y+yoff, self.w, self.h);
-    for(var i = 0; i < view.zones.length; i++)
+    ctx.drawImage(self.cur_view.img, self.x, self.y+yoff, self.w, self.h);
+    for(var i = 0; i < self.cur_view.zones.length; i++)
     {
-      zone = view.zones[i];
+      zone = self.cur_view.zones[i];
       ctx.drawImage(zone.img, zone.x, zone.y+yoff, zone.w, zone.h);
     }
 
+    //debug
     ctx.strokeStyle = white;
-    for(var i = 0; i < view.zones.length; i++)
+    for(var i = 0; i < self.cur_view.zones.length; i++)
     {
-      zone = view.zones[i];
+      zone = self.cur_view.zones[i];
       ctx.strokeRect(zone.x, zone.y+yoff, zone.w, zone.h);
     }
     ctx.strokeRect(self.x, self.y+yoff, self.w, self.h);
     ctx.strokeRect(self.exit_box.x, self.exit_box.y+yoff, self.exit_box.w, self.exit_box.h);
+    if(self.cur_view_i > 0) ctx.strokeRect(self.prev_box.x, self.prev_box.y+yoff, self.prev_box.w, self.prev_box.h);
+    if(self.cur_view_i < self.cache_unlocked_views.length-1) ctx.strokeRect(self.next_box.x, self.next_box.y+yoff, self.next_box.w, self.next_box.h);
   }
 }
 
