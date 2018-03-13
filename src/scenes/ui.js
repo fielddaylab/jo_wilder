72111a06417d11f9a28b4231c2557496dbbc7dcf
Phildo <phildo211@gmail.com>
2018-03-13 10:40:04 -0500

    uncommitted stuff from other computer

--- src/scenes/ui.js
+++ src/scenes/ui.js
@@ -692,10 +692,15 @@ var navigable = function()
   self.tick = function()
   {
     //move camera
+    var lerp_s = 0.03;
     var xp = invlerp(self.room.wx-self.room.ww/2, self.room.wx+self.room.ww/2, my_avatar.wx);
     var camwd = self.room.ww-my_camera.ww;
     var target_cam_wx = self.room.wx-camwd/2+xp*camwd;
-    my_camera.wx = lerp(my_camera.wx,target_cam_wx,0.01);
+    my_camera.wx = lerp(my_camera.wx,target_cam_wx,lerp_s);
+    var yp = invlerp(self.room.wy-self.room.wh/2, self.room.wy+self.room.wh/2, my_avatar.wy);
+    var camhd = self.room.wh-my_camera.wh;
+    var target_cam_wy = self.room.wy-camhd/2+yp*camwd;
+    my_camera.wy = lerp(my_camera.wy,target_cam_wy,lerp_s);
 
     self.room.animcycle_inst.tick();
     screenSpace(my_camera,canv,self.room);
