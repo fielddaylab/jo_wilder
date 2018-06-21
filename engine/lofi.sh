d22364f91a12e456b396e9bf9ac425f08dc736e8
Phildo <phildo211@gmail.com>
2018-06-21 11:36:54 -0500

    newly converted lofi

--- engine/lofi.sh
+++ engine/lofi.sh
@@ -2,5 +2,15 @@
 
 cd ../assets/data/levels/tunic
 
-for i in animcycles/*; do if [ -d $i ]; then echo $i; for j in $i/*; do echo $j; convert $j -resize %50 `echo $j | sed 's/animcycles/lofianimcycles/'`; done; fi; done;
+for i in animcycles/*; do
+  if [ -d $i ]; then
+    if [ ! -d `echo $i | sed 's/animcycles/lofianimcycles/'` ]; then
+      mkdir `echo $i | sed 's/animcycles/lofianimcycles/'`
+    fi
+    for j in $i/*; do
+      echo $j...
+      convert $j -resize %50 `echo $j | sed 's/animcycles/lofianimcycles/'`
+    done
+  fi
+done
 
